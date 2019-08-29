afa0b8a5ac11c16cff92e7bf56036a6780bcac6c
FinancialRADDeveloper <alan_russell@hotmail.com>
2019-08-29 21:05:24 +0000

    Further enhancements to activity stats, splitting into listen to, skipped and search activity and provide 24 chunked activity ready to feed into matplotlib pyplot

--- activity_parser.py
+++ activity_parser.py
@@ -38,18 +38,24 @@ def get_unique_vals_for_each_column(dataframe):
     return unique_vals_for_each_column
 
 
+def filter_out_skipped(activity_data_frame):
+
+    listened_to_dataframe = activity_data_frame[activity_data_frame['title'].str.contains("Listened to")]
+
+    skipped_tracks_dataframe = activity_data_frame[activity_data_frame['title'].str.contains("Skipped")]
+
+    searched_activity_dataframe = activity_data_frame[activity_data_frame['title'].str.contains("Searched")]
+
+    other_activity_dataframe = activity_data_frame[~activity_data_frame['title'].str.contains("Listened to")]
+    other_activity_dataframe = other_activity_dataframe[~other_activity_dataframe['title'].str.contains("Skipped")]
+
+    return listened_to_dataframe
+
 def calculate_hourly_activity(activity_data_frame):
-    from dateutil import parser
-    from datetime import datetime
-
-    dt = parser.parse("2019-08-20T16:49:54.510Z")
-    # datetime_object = datetime.strptime('', '%b %d %Y %I:%M%p')
-
-    # this is going to be really slow but gets me to move on a come back to later
 
     string_activity_dates = activity_data_frame['time'].tolist()
 
-    converted_dates = []
+    # converted_dates = []
 
     # for i, elem in enumerate(string_activity_dates):
     #     converted_dates.append(parser.parse(elem))
@@ -61,13 +67,35 @@ def calculate_hourly_activity(activity_data_frame):
 
     activity_data_frame['hour'] = activity_data_frame['activity_datetime'].dt.hour
 
-    listening_freq_by_hour = activity_data_frame['hour'].value_counts()
+    listened_to_dataframe = activity_data_frame[activity_data_frame['title'].str.contains("Listened to")]
+    skipped_tracks_dataframe = activity_data_frame[activity_data_frame['title'].str.contains("Skipped")]
+    searched_activity_dataframe = activity_data_frame[activity_data_frame['title'].str.contains("Searched")]
 
-    xx = list(zip(listening_freq_by_hour.index.tolist(),listening_freq_by_hour.tolist()))
+    listening_freq_by_hour = listened_to_dataframe['hour'].value_counts()
+    skipped_freq_by_hour = skipped_tracks_dataframe['hour'].value_counts()
+    search_freq_by_hour = searched_activity_dataframe['hour'].value_counts()
 
-    listening_freq_by_hour = sorted(xx)
+    daily_listening_stats = sorted(list(zip(listening_freq_by_hour.index.tolist(),listening_freq_by_hour.tolist())))
+    skipped_stats = sorted(list(zip(skipped_freq_by_hour.index.tolist(), skipped_freq_by_hour.tolist())))
+    search_stats = sorted(list(zip(search_freq_by_hour.index.tolist(), search_freq_by_hour.tolist())))
 
-    # xx = sorted(xx, 0)
+    import matplotlib.pyplot as plt
+
+
+    plt.ion()
+
+    # plt.style.use('ggplot')
+
+    plt.bar([i[0] for i in daily_listening_stats], [j[1] for j in daily_listening_stats], color = 'blue')
+    plt.bar([i[0] for i in skipped_stats], [j[1] for j in skipped_stats], color='red')
+    plt.bar([i[0] for i in search_stats], [j[1] for j in search_stats], color='purple')
+    plt.xlabel('Hour of the Day')
+    plt.ylabel('Total tracks listened')
+
+    plt.show()
+    plt.close()
+
+
     # first we need to convert the column into a date time
     # activity_data_frame['ActivityDateTime'] = pd.to_datetime(activity_data_frame['time'],
     #                                                        format='%Y-%m-%d %H:%M:%S-%Z',
@@ -89,6 +117,8 @@ def main():
         print("Attempting to load JSON to DataFrame")
         gmusic_activity_df = load_json_to_dataframe()
 
+        new_df = filter_out_skipped(gmusic_activity_df)
+
         calculate_hourly_activity(gmusic_activity_df)
 
         unique_vals = get_unique_vals_for_each_column(gmusic_activity_df)
@@ -97,6 +127,7 @@ def main():
     except Exception as e:
 
         print(e)
+
 
 if __name__ == "__main__":
     main()
